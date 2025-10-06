45d296294d3b6fb10c594cc58e22468374e823a9
jaimejover <jaimejover8@gmail.com>
2025-10-06 19:05:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch('https://api-open.data.gov.sg/v2/real-time/api/psi', {
+fetch('https://data.gov.sg/datasets/d_fe37906a0182569d891506e815e819b7/view#tag/default/GET/psi', {
   headers: {
     'X-Api-Key': 'YOUR_SECRET_TOKEN'
   }
